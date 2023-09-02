283124c5d930073a1683f0d24754aa90699a51f6
Naya070 <nayarith070@gmail.com>
2023-09-02 13:15:49 -0400

    Inyección con use value

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
-import { Module } from '@nestjs/common';
+import { HttpService, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = '12345634';
+const API_KEY_PROD = '123PROD';
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
